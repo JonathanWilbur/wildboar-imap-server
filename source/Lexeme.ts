9f3c72ef95ce5373f64875a452ebb00ddcab46a1
Jonathan M. Wilbur <jonathan@wilbur.space>
2019-03-24 00:29:36 -0400

    Removed commented code

--- source/Lexeme.ts
+++ source/Lexeme.ts
@@ -17,31 +17,4 @@ class Lexeme {
             default: return "";
         }
     }
-
-    // public getTag () : string {
-    //     if (this.type !== LexemeType.COMMAND_LINE) return "";
-    //     if (this.token.length === 0) return "";
-    //     const indexOfFirstSpace : number = this.token.indexOf(" ");
-    //     const command : Buffer = ((indexOfFirstSpace === -1) ?
-    //         this.token : this.token.slice(0, indexOfFirstSpace));
-    //     return command.toString();
-    // }
-
-    // public getCommand () : string {
-    //     if (this.type !== LexemeType.COMMAND_LINE) return "";
-    //     if (this.token.length === 0) return "";
-    //     const subtokens : string[] = this.token.toString().split(/\s+/);
-    //     if (subtokens.length < 2) return "";
-    //     return subtokens[1];
-    // }
-
-    // public getArguments () : string {
-    //     if (this.type !== LexemeType.COMMAND_LINE) return "";
-    //     if (this.token.length === 0) return "";
-    //     const indexOfFirstSpace : number = this.token.indexOf(" ");
-    //     if (indexOfFirstSpace === -1) return "";
-    //     const indexOfSecondSpace : number = this.token.indexOf(" ", indexOfFirstSpace + 1);
-    //     if (indexOfSecondSpace === -1) return "";
-    //     return this.token.toString().slice(indexOfSecondSpace);
-    // }
 }
